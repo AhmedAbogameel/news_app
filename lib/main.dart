f0d75e6e226a30f759240b79a0a12ec613cd2ea8
AhmedAbogameel <gamilco81@gmail.com>
2020-08-21 03:57:22 +0200

    News App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:news_app/providers/news_provider.dart';
 import 'package:news_app/screens/all_news_screen.dart';
 import 'package:news_app/screens/news_details_screen.dart';
