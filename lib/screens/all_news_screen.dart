96fa37b8c8f546034c133ddf53730e0642774c6e
AhmedAbogameel <gamilco81@gmail.com>
2020-08-21 14:50:53 +0200

    Breaking News

--- lib/screens/all_news_screen.dart
+++ lib/screens/all_news_screen.dart
@@ -6,55 +6,43 @@ import 'package:news_app/providers/news_provider.dart';
 import 'package:news_app/widgets/big_card.dart';
 import 'package:provider/provider.dart';
 
-class AllNewsScreen extends StatefulWidget {
+class AllNewsScreen extends StatelessWidget {
   static const String routeName = '/all_news_screen';
-
-  @override
-  _AllNewsScreenState createState() => _AllNewsScreenState();
-}
-
-class _AllNewsScreenState extends State<AllNewsScreen> {
-//  Future<List<NewsModel>> future;
-//
-//  @override
-//  void initState() {// ????????????????
-//    Future.delayed(Duration.zero).then((_) =>
-//        future = Provider.of<NewsProvider>(context,listen: false).fetchNews()
-//    );
-//    super.initState();
-//  }
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Home',style: TextStyle(color: kWhiteColor),),
-        backgroundColor: kPrimaryColor,
-      ),
-      child: Consumer<NewsProvider>(
-        builder: (ctx,provider,child){
-          final Future<List<NewsModel>> future = provider.fetchNews();
-          return SafeArea(
-          child: FutureBuilder<List<NewsModel>>(
-            future: future,
-            builder: (ctx, snapshot) => snapshot.hasData
-                ? ListView.builder(
-                itemCount: snapshot.data.length,
-                padding: const EdgeInsets.all(8.0),
-                itemBuilder: (ctx, index) {
-                  NewsModel newsModel = snapshot.data[index];
-                  return BigCard(
-                    title: newsModel.title,
-                    publishAt: newsModel.publishedAt,
-                    urlToImage: newsModel.urlToImage,
-                    newsModel:newsModel,
-                  );
-                })
-                : Center(
-              child: CupertinoActivityIndicator(),),
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(
+            'Home',
+            style: TextStyle(color: kWhiteColor),
           ),
-        );},
-      )
-    );
+          backgroundColor: kPrimaryColor,
+        ),
+        child: Consumer<NewsProvider>(
+          builder: (ctx, provider, child) {
+            final Future<List<NewsModel>> future = provider.fetchNews();
+            return SafeArea(
+              child: FutureBuilder<List<NewsModel>>(
+                future: future,
+                builder: (ctx, snapshot) => snapshot.hasData
+                    ? ListView.builder(
+                        itemCount: snapshot.data.length,
+                        padding: const EdgeInsets.all(8.0),
+                        itemBuilder: (ctx, index) {
+                          NewsModel newsModel = snapshot.data[index];
+                          return BigCard(
+                            title: newsModel.title,
+                            publishAt: newsModel.publishedAt,
+                            urlToImage: newsModel.urlToImage,
+                            newsModel: newsModel,
+                          );
+                        })
+                    : Center(
+                        child: CupertinoActivityIndicator(),
+                      ),
+              ),
+            );
+          },
+        ));
   }
 }
